8d1e3b62550ac6d3b06e2e3c5e667c98945e12ca
troy <vitalii.proskura@gmail.com>
2017-11-23 21:38:49 +0200

    Refactored TestProcessor as final

--- psycorp-core/src/main/java/psycorp/core/util/TestProcessor.java
+++ psycorp-core/src/main/java/psycorp/core/util/TestProcessor.java
@@ -10,7 +10,7 @@ import java.util.*;
 /**
  * Created by User on 19.11.2017.
  */
-public class TestProcessor {
+public final class TestProcessor {
 
     public static boolean isPassed (TestCore test) {
         return test.getMatches().stream().noneMatch(match -> match.getAnswer() == Answer.NAN);
